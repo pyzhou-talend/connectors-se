ae8af3784726d643e64a030b880c63e37a7764d7
Fabien Désiles <52663885+fdesiles@users.noreply.github.com>
2022-12-02 09:51:02 +0100

    chore(TDI-48973): Fix Sonar NoClassDefFoundError (#973)
    https://jira.talendforge.org/browse/TDI-48973
    
    Following SFT bump of Sonar in 9.6, builds are failing with:
    Exception in thread "main" java.lang.NoClassDefFoundError: com/google/common/collect/LinkedListMultimap$1KeySetImpl
     
    This is linked to a bug on 9.6 using Sonar cache.
    
    It is fixed in 9.7.1, however there is another blocker for SFT to stay on version 9.6 for the time being.

--- .jenkins/build.sh
+++ .jenkins/build.sh
@@ -36,12 +36,14 @@ main() (
     declare -a LIST_FILE_ARRAY=( $(find $(pwd) -type f -name 'jacoco.xml') )
     LIST_FILE=$(IFS=, ; echo "${LIST_FILE_ARRAY[*]}")
     # Why sonar plugin is not declared in pom.xml: https://blog.sonarsource.com/we-had-a-dream-mvn-sonarsonar
+    # TODO https://jira.talendforge.org/browse/TDI-48980 (CI: Reactivate Sonar cache)
     mvn sonar:sonar \
         --define 'sonar.host.url=https://sonar-eks.datapwn.com' \
         --define "sonar.login=${SONAR_LOGIN}" \
         --define "sonar.password=${SONAR_PASSWORD}" \
         --define "sonar.branch.name=${branch}" \
         --define "sonar.coverage.jacoco.xmlReportPaths='${LIST_FILE}'" \
+        --define "sonar.analysisCache.enabled=false" \
         --activate-profiles SONAR \
         "${extraBuildParams[@]}"
   fi
